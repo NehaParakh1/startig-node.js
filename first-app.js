916e8e25df6df4d8b69770c293fcd23520368360
NehaParakh1 <nehadugar3@gmail.com>
2023-07-20 10:39:32 +0530

    creating server

--- first-app.js
+++ first-app.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const http=require('http');
+
+const name='Neha Parakh';
+
+const server= http.createServer((req, res)=>{
+ console.log(req)
+    console.log(name);
+})
+
+server.listen(4000);
